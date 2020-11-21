9c8e15fbe2c20d62f119f8532019a12d23c628d4
PunkCat34 <taha.topal.7@gmail.com>
2020-11-21 15:08:20 +0300

    deploy fix

--- Booking/src/main/java/com/project/book/repo/BookRepository.java
+++ Booking/src/main/java/com/project/book/repo/BookRepository.java
@@ -1,2 +1,12 @@
-package com.project.book.repo;public interface BookRepository {
+package com.project.book.repo;
+
+import com.project.book.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.UUID;
+
+public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    List<Book> findByGoogleBooksId(String googleBooksId);
+
 }
