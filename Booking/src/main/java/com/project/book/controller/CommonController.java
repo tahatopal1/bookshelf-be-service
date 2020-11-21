3b588852c48ceaa17e583295b68e6f49eb663e47
PunkCat34 <taha.topal.7@gmail.com>
2020-11-21 16:52:33 +0300

    CommonController fix

--- Booking/src/main/java/com/project/book/controller/CommonController.java
+++ Booking/src/main/java/com/project/book/controller/CommonController.java
@@ -1,2 +1,23 @@
-package com.project.book.controller;public class CommonController {
+package com.project.book.controller;
+
+import com.project.book.service.CommonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/common")
+public class CommonController {
+
+    @Autowired
+    private CommonService commonService;
+
+    @GetMapping(value = "/menu-titles", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> menuTitles(@RequestParam int session){
+        return ResponseEntity.ok(commonService.menuTitles(session));
+    }
+
 }
